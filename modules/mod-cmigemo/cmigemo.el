02a1154aaaadb544accc4022daf795d63ccefc45
HIROSHI OOTA <h2oota@users.noreply.github.com>
2016-03-01 18:16:26 +0900

    Don't save result of regexp conversion

--- modules/mod-cmigemo/cmigemo.el
+++ modules/mod-cmigemo/cmigemo.el
@@ -27,7 +27,7 @@
 
 (require 'character-fold)
 (require 'mod-cmigemo)
-(isearch-define-mode-toggle cmigemo "m" cmigemo-search-pattern-get "\
+(isearch-define-mode-toggle cmigemo "m" cmigemo-get-pattern "\
 Turning on cmigemo search turns off regexp mode.")
 
 ;;; Code:
@@ -39,11 +39,6 @@ Turning on cmigemo search turns off regexp mode.")
   "if t, detect buffer's coding system and choose migemo or search-default-mode, "
   :group 'cmigemo
   :type 'boolean)
-
-(defcustom cmigemo-white-space-regexp "[ 　\t\r\n]*"
-  "*Regexp representing white spaces."
-  :group 'cmigemo
-  :type 'string)
 
 (defcustom cmigemo-coding-system 'utf-8-unix
   "*Default coding system for cmigemo.el"
@@ -86,31 +81,6 @@ Turning on cmigemo search turns off regexp mode.")
   :type '(choice (const :tag "Do not use" nil)
 		 function))
 
-(defcustom cmigemo-use-pattern-alist nil
-  "*Use pattern cache."
-  :group 'cmigemo
-  :type 'boolean)
-
-(defcustom cmigemo-use-frequent-pattern-alist nil
-  "*Use frequent patttern cache."
-  :group 'cmigemo
-  :type 'boolean)
-
-(defcustom cmigemo-pattern-alist-length 512
-  "*Maximal length of cmigemo-pattern-alist."
-  :group 'cmigemo
-  :type 'integer)
-
-(defcustom cmigemo-pattern-alist-file "~/.cmigemo-pattern"
-  "*Path of cmigemo alist file. If nil, don't save and restore the file."
-  :group 'cmigemo
-  :type 'file)
-
-(defcustom cmigemo-frequent-pattern-alist-file "~/.cmigemo-frequent"
-  "*Path of cmigemo frequent alist file. If nil, don't save and restore the file."
-  :group 'cmigemo
-  :type 'file)
-
 (defcustom cmigemo-isearch-min-length 1
   "*Minimum length of word to start isearch."
   :group 'cmigemo
@@ -120,9 +90,6 @@ Turning on cmigemo search turns off regexp mode.")
 (defvar cmigemo-buffer nil)
 (defvar cmigemo-current-input-method nil)
 (defvar cmigemo-search-pattern nil)
-(defvar cmigemo-pattern-alist nil)
-(defvar cmigemo-frequent-pattern-alist nil)
-(defvar cmigemo-search-pattern-alist nil)
 (defvar cmigemo-do-isearch nil)
 (defvar *mod-cmigemo-handle* nil)
 
@@ -142,14 +109,6 @@ Turning on cmigemo search turns off regexp mode.")
       (lambda (c)
 	  (intern-soft (concat (symbol-name c) "-mac"))) cs))))
 
-(defun cmigemo-search-pattern-get (string &optional lax)
-  (or (cdr (assoc string cmigemo-search-pattern-alist))
-      (let ((pattern (cmigemo-get-pattern string)))
-        (setq cmigemo-search-pattern-alist
-              (cons (cons string pattern)
-                    cmigemo-search-pattern-alist))
-        pattern)))
-
 (defun cmigemo-replace-in-string (string from to)
   (with-temp-buffer
     (insert string)
@@ -159,175 +118,41 @@ Turning on cmigemo search turns off regexp mode.")
         (replace-match to nil t)))
     (buffer-substring (point-min) (point-max))))
 
-(defun cmigemo-get-pattern (word)
+(defun cmigemo-get-pattern (word &optional lax)
   (if (< (length word) cmigemo-isearch-min-length)
-      ""
-
-;      (set-text-properties 0 (length word) nil word)
-      (cmigemo-init)
-      (when (and cmigemo-pre-conv-function
-		 (functionp cmigemo-pre-conv-function))
-	(setq word (funcall cmigemo-pre-conv-function word)))
-      (let ((pattern
-             (let (temp)
-               (cond
-                ((setq temp (and cmigemo-use-frequent-pattern-alist
-                                 (assoc word cmigemo-frequent-pattern-alist)))
-                 (cdr temp))
-                ((setq temp (and cmigemo-use-pattern-alist
-                                 (assoc word cmigemo-pattern-alist)))
-                 (setq cmigemo-pattern-alist
-                       (cons temp (delq temp cmigemo-pattern-alist)))
-                 (cdr temp))
-                (t
-                 (let ((pattern (cmigemo-query *mod-cmigemo-handle* word)))
-                   (when cmigemo-use-pattern-alist
-                     (setq cmigemo-pattern-alist
-                           (cons (cons word pattern) cmigemo-pattern-alist))
-                     (when (and cmigemo-pattern-alist-length
-                                (> (length cmigemo-pattern-alist)
-                                   (* cmigemo-pattern-alist-length 2)))
-                       (setcdr (nthcdr (1- (* cmigemo-pattern-alist-length 2))
-                                       cmigemo-pattern-alist) nil)))
-                   pattern))))))
-        (if (and cmigemo-after-conv-function
-                 (functionp cmigemo-after-conv-function))
-            (funcall cmigemo-after-conv-function word pattern)
-          (cmigemo-replace-in-string pattern "\a"
-                                     cmigemo-white-space-regexp)))))
+      word
+    (let ((word (if (and cmigemo-pre-conv-function
+                         (functionp cmigemo-pre-conv-function))
+                    (funcall cmigemo-pre-conv-function word)
+                  word))
+          (pattern
+           (progn
+             (cmigemo-init)
+             (cmigemo-query *mod-cmigemo-handle* word))))
+      (if (and cmigemo-after-conv-function
+               (functionp cmigemo-after-conv-function))
+          (funcall cmigemo-after-conv-function word pattern)
+        pattern))))
 
 (defun cmigemo-init ()
-  (when (and cmigemo-use-frequent-pattern-alist
-	     cmigemo-frequent-pattern-alist-file
-	     (null cmigemo-frequent-pattern-alist))
-    (setq cmigemo-frequent-pattern-alist
-	  (cmigemo-pattern-alist-load cmigemo-frequent-pattern-alist-file)))
-
-  (when (and cmigemo-use-pattern-alist
-	     cmigemo-pattern-alist-file
-	     (null cmigemo-pattern-alist))
-    (setq cmigemo-pattern-alist
-	  (cmigemo-pattern-alist-load cmigemo-pattern-alist-file)))
-
-  (setq *mod-cmigemo-handle*
-        (cmigemo-open cmigemo-dictionary)))
-
-(defun cmigemo-pattern-alist-load (file)
-  "Load cmigemo alist file."
-  (let ((coding-system-for-read cmigemo-coding-system))
-    (setq file (expand-file-name file))
-    (when (file-readable-p file)
-      (with-temp-buffer
-	(insert-file-contents file)
-	(goto-char (point-min))
-	(condition-case err
-	    (read (current-buffer))
-	  (error
-	   (message "Error while reading %s; %s"
-		    (file-name-nondirectory file)
-		    (error-message-string err))
-	   nil))))))
-
-(defun cmigemo-pattern-alist-save (&optional clear)
-  "Save cmigemo alist file."
-  (interactive)
-  (when (and cmigemo-use-pattern-alist
-	     cmigemo-pattern-alist-file
-	     (or cmigemo-pattern-alist clear))
-    (let ((file (expand-file-name cmigemo-pattern-alist-file))
-	  (coding-system-for-write cmigemo-coding-system))
-      (when (file-writable-p file)
-	(when clear
-	  (setq cmigemo-pattern-alist nil))
-	(when (and cmigemo-pattern-alist-length
-		   (> (length cmigemo-pattern-alist) cmigemo-pattern-alist-length))
-	  (setcdr (nthcdr (1- cmigemo-pattern-alist-length)
-			  cmigemo-pattern-alist) nil))
-	(with-temp-buffer
-	  (if (fboundp 'pp)
-	      (pp cmigemo-pattern-alist (current-buffer))
-	    (prin1 cmigemo-pattern-alist (current-buffer)))
-	  (write-region (point-min) (point-max) file nil 'nomsg))
-	(setq cmigemo-pattern-alist nil)))))
+  (unless *mod-cmigemo-handle*
+    (setq *mod-cmigemo-handle*
+          (cmigemo-open cmigemo-dictionary))))
 
 (defun cmigemo-kill ()
   "Kill cmigemo process"
   (interactive)
-  ;; mod-cmigemo begin
   (when *mod-cmigemo-handle*
     (cmigemo-close *mod-cmigemo-handle*)
     (setq *mod-cmigemo-handle* nil)))
 
-(defun cmigemo-pattern-alist-clear ()
-  "Clear cmigemo alist data & file."
-  (interactive)
-  (cmigemo-kill)
-  (cmigemo-pattern-alist-save 'clear)
-  (cmigemo-init))
-
-(defun cmigemo-frequent-pattern-make (fcfile)
-  "Create frequent pattern from `frequent-chars'."
-  (interactive "ffrequent-chars: ")
-  (cmigemo-pattern-alist-save 'clear)
-  (when cmigemo-frequent-pattern-alist-file
-    (cmigemo-kill)
-    (cmigemo-init)
-    (let ((file (expand-file-name cmigemo-frequent-pattern-alist-file))
-	  (coding-system-for-write cmigemo-coding-system)
-	  (file-coding-system  cmigemo-coding-system)
-	  (cmigemo-use-pattern-alist nil)
-	  (cmigemo-use-frequent-pattern-alist nil)
-	  (cmigemo-after-conv-function (lambda (x y) y))
-	  word regex)
-      (setq cmigemo-frequent-pattern-alist nil)
-      (with-temp-buffer
-	(insert-file-contents fcfile)
-	(goto-char (point-min))
-	(message "Make frequently pattern...")
-	(while (not (eobp))
-	  (when (looking-at "^[a-z]+$")
-	    (setq word (match-string 0))
-	    (message "Make frequently pattern...%s" word)
-	    (setq cmigemo-frequent-pattern-alist
-		  (cons (cons word (cmigemo-get-pattern word))
-			cmigemo-frequent-pattern-alist)))
-	  (forward-line 1))
-	(when (file-writable-p file)
-	  (setq cmigemo-frequent-pattern-alist
-		(nreverse cmigemo-frequent-pattern-alist))
-	  (erase-buffer)
-	  (if (fboundp 'pp)
-	      (pp cmigemo-frequent-pattern-alist (current-buffer))
-	    (prin1 cmigemo-frequent-pattern-alist (current-buffer)))
-	  (write-region (point-min) (point-max) file nil 'nomsg)))
-      (cmigemo-kill)
-      (cmigemo-init)
-      (message "Make frequently pattern...done"))))
-
-(defun cmigemo-expand-pattern () "\
-Expand the Romaji sequences on the left side of the cursor
-into the cmigemo's regexp pattern."
-  (interactive)
-  (let ((pos (point)))
-    (goto-char (- pos 1))
-    (if (re-search-backward "[^-a-zA-Z]" (line-beginning-position) t)
-	(forward-char 1)
-      (beginning-of-line))
-    (let* ((str (buffer-substring-no-properties (point) pos))
-	   (jrpat (cmigemo-get-pattern str)))
-      (delete-region (point) pos)
-      (insert jrpat))))
-
-;;
-;;
-;;
 (advice-add 'isearch-mode :around
   (lambda (func &rest args)
     (let ((search-default-mode
            (if (and cmigemo-detect-default-mode
                     (japanese-p (detect-coding-with-language-environment
                                  (point-min) (point-max) "japanese")))
-               #'cmigemo-search-pattern-get
+               #'cmigemo-get-pattern
              search-default-mode )))
       (apply func args)))
   '((name . cmigemo-isearch-mode-advice)))
@@ -337,16 +162,6 @@ into the cmigemo's regexp pattern."
 	((member (car coding-system-list)
 		 coding-system-for-japanese) t)
 	(t (japanese-p (cdr coding-system-list)))))
-
-(add-hook 'isearch-mode-hook
-  (lambda ()
-    (setq cmigemo-search-pattern nil
-          cmigemo-search-pattern-alist nil)))
-
-(add-hook 'isearch-mode-end-hook
-  (lambda ()
-    (setq cmigemo-search-pattern nil
-          cmigemo-search-pattern-alist nil)))
 
 ;; experimental
 (define-key global-map (kbd "C-M-;") #'cmigemo-dabbrev-expand)
@@ -368,9 +183,6 @@ into the cmigemo's regexp pattern."
 (defvar cmigemo-dabbrev-ol nil)
 (defun cmigemo-dabbrev-expand-done ()
   (remove-hook 'pre-command-hook 'cmigemo-dabbrev-expand-done)
-  (unless (eq last-command this-command)
-      (setq cmigemo-search-pattern-alist nil)
-      (setq cmigemo-dabbrev-pre-patterns nil))
   (when cmigemo-dabbrev-ol
     (delete-overlay cmigemo-dabbrev-ol)))
 
@@ -379,7 +191,7 @@ into the cmigemo's regexp pattern."
   (setq cmigemo-search-pattern
         (if (delq 'ascii (find-charset-string word))
             word
-          (cmigemo-search-pattern-get word)))
+          (cmigemo-get-pattern word)))
   (search-forward-regexp cmigemo-search-pattern bound noerror count))
 
 (defun cmigemo-backward (word &optional bound noerror count)
@@ -387,7 +199,7 @@ into the cmigemo's regexp pattern."
   (setq cmigemo-search-pattern
         (if (delq 'ascii (find-charset-string word))
             word
-          (cmigemo-search-pattern-get word)))
+          (cmigemo-get-pattern word)))
 
   (if (null cmigemo-do-isearch)
       (search-backward-regexp cmigemo-search-pattern bound noerror count)
@@ -411,7 +223,6 @@ into the cmigemo's regexp pattern."
       (if (re-search-backward "[^a-z-]" (line-beginning-position) t)
 	  (forward-char 1)
 	(beginning-of-line))
-      (setq cmigemo-search-pattern-alist nil)
       (setq cmigemo-dabbrev-start-point (point))
       (setq cmigemo-dabbrev-search-point (point))
       (setq cmigemo-dabbrev-pattern
